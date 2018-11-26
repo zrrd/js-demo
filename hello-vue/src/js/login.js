0b543b7ef90627f3326b994dfea66a7afacfa9e8
邵益炯 <shaoyijiong@worken.cn>
2018-11-26 18:13:31 +0800

    feat(vue):vue-router

---
+++ hello-vue/src/js/login.js
@@ -0,0 +1,8 @@
+const loginForm = {
+  template: `
+  <div>
+  <h1>登录页</h1>
+    用户名: <input type="text"><br>
+    密码: <input type="password"><br>
+  </div>`
+};
---
+++ hello-vue/src/js/regiser.js
@@ -0,0 +1,9 @@
+const registerForm = {
+  template: `
+   <div>
+     <h1>注册页</h1>
+    用户名: <input type="text"><br>
+    密码: <input type="password"><br>
+    确认密码: <input type="password"><br>
+  </div>`
+};
